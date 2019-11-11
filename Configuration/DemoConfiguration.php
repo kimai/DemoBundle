f1132daaecd2ae65408c9611862714c93944823a
Kevin Papst <kpapst@gmx.net>
2019-11-11 17:39:08 +0100

    showcase the configuration system

--- Configuration/DemoConfiguration.php
+++ Configuration/DemoConfiguration.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the Kimai time-tracking app.
+ * This file is part of the Kimai DemoBundle.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
