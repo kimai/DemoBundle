ff700ff99aa80af45c2140e3133f2145b601821e
Kevin Papst <kpapst@gmx.net>
2020-02-22 23:35:00 +0100

    added plugin screen features

---
+++ EventSubscriber/PluginActionsSubscriber.php
@@ -0,0 +1,82 @@
+<?php
+
+/*
+ * This file is part of the KioskBundle for Kimai 2.
+ * All rights reserved by Kevin Papst (www.kevinpapst.de).
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace KimaiPlugin\DemoBundle\EventSubscriber;
+
+use App\Entity\User;
+use App\Event\ThemeEvent;
+use App\Plugin\Plugin;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+final class PluginActionsSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private $router;
+    /**
+     * @var AuthorizationCheckerInterface
+     */
+    private $security;
+
+    /**
+     * @param UrlGeneratorInterface $router
+     */
+    public function __construct(UrlGeneratorInterface $router, AuthorizationCheckerInterface $security)
+    {
+        $this->router = $router;
+        $this->security = $security;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'actions.plugin' => ['onPluginEvent'],
+        ];
+    }
+
+    public function onPluginEvent(ThemeEvent $event)
+    {
+        $payload = $event->getPayload();
+
+        if (!isset($payload['actions']) || !isset($payload['plugin'])) {
+            return;
+        }
+
+        /** @var Plugin $plugin */
+        $plugin = $payload['plugin'];
+
+        if ($plugin->getId() !== 'DemoBundle') {
+            return;
+        }
+
+        if (!$this->security->isGranted('demo')) {
+            return;
+        }
+
+        $payload['actions']['divider'] = null;
+
+        $payload['actions']['settings'] = [
+            'url' => $this->router->generate('system_configuration_section', ['section' => 'demo_config']),
+            'class' => 'modal-ajax-form',
+        ];
+
+        $payload['actions']['details'] = [
+            'url' => $this->router->generate('demo', []),
+        ];
+
+        $event->setPayload($payload);
+    }
+}
